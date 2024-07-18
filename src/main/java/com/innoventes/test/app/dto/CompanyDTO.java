11a9e568a036b0a1032d7d19a12df342daceb1b1
Prathap473 <pratapsv02@gmail.com>
2024-07-18 17:06:51 +0530

    Update CompanyDTO.java
    Added constraints

--- src/main/java/com/innoventes/test/app/dto/CompanyDTO.java
+++ src/main/java/com/innoventes/test/app/dto/CompanyDTO.java
@@ -13,11 +13,19 @@ public class CompanyDTO {
 
 	private Long id;
 
+	@NotNull(message = "Company name is mandatory")
+	@NotEmpty("message = "Company name cannot be empty")
+	@Size(min = 5, message = "Companyname should be at least 5 characters long")
 	private String companyName;
 
+	@NoNull(message = "Email is manadatory")
+	@Email(message = "Email should be valid")
 	private String email;
+	
 
+	@PositiveOrZero(message = "Strength should be a positive number or zero")
 	private Integer strength;
+
 	
 	private String webSiteURL;
 }
