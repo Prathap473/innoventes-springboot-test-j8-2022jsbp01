f65310b9a5d022af84bbe607d551b9ad99692b90
Prathap473 <pratapsv02@gmail.com>
2024-07-18 17:17:46 +0530

    Update CompanyDTO.java

--- src/main/java/com/innoventes/test/app/dto/CompanyDTO.java
+++ src/main/java/com/innoventes/test/app/dto/CompanyDTO.java
@@ -10,14 +10,26 @@ import lombok.Setter;
 @Setter
 @Getter
 public class CompanyDTO {
-
+        @Id
+	@SequenceGenerator(sequenceName = "company_seq", allocationSize = 1, name = "company_seq")
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "company_seq")
 	private Long id;
 
+	@Column(name = "company_name")
+	@NotNull(message = "Company name is mandatory")
+	@NotEmpty("message = "Company name cannot be empty")
+	@Size(min = 5, message = "Companyname should be at least 5 characters long")
 	private String companyName;
 
+	@Column(name = "email")
+	@NotNull(message = "Email is mandatory")
+	@EMail(message = "Email should be valid")
 	private String email;
 
+	@Column(name = "strength")
+
+	@Column(name = "Strength")
+	@PositiveOrZero(message = "Strength should be a positive number or zero")
 	private Integer strength;
-	
-	private String webSiteURL;
-}
+
+	@Column(name = "website_url")
