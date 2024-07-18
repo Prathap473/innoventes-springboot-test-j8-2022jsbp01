9a1869929af6c273949baaa6311c7bf9989160ad
Prathap473 <pratapsv02@gmail.com>
2024-07-18 17:13:01 +0530

    Update CompanyDTO.java
    Added Constraints

--- src/main/java/com/innoventes/test/app/dto/CompanyDTO.java
+++ src/main/java/com/innoventes/test/app/dto/CompanyDTO.java
@@ -11,13 +11,28 @@ import lombok.Setter;
 @Getter
 public class CompanyDTO {
 
+	
+	@Id
+	@SequenceGenerator(sequenceName = "company_seq", allocationSize = 1, name = "company_seq")
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "company_seq")
 	private Long id;
+	
 
+	
+	@NotNull(message = "Company name is mandatory")
+	@NotEmpty("message = "Company name cannot be empty")
+	@Size(min = 5, message = "Companyname should be at least 5 characters long")
 	private String companyName;
 
+	@Column(name = "email")
+	@NotNull(message = "Email is mandatory")
+	@EMail(message = "EMail should be valid")
 	private String email;
 
+	@Column(name = "Strength")
+	@PositiveOrZero(message = "Strength should be a positive number or zero")
 	private Integer strength;
-	
+
+	@Column(name = "website_url")
 	private String webSiteURL;
 }
