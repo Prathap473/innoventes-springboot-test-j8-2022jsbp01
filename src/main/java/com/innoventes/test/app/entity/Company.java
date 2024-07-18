448806442504fe459b66fcc978f7afd19b5165c5
Prathap473 <pratapsv02@gmail.com>
2024-07-18 17:15:52 +0530

    Update Company.java

--- src/main/java/com/innoventes/test/app/entity/Company.java
+++ src/main/java/com/innoventes/test/app/entity/Company.java
@@ -31,12 +31,18 @@ public class Company extends BaseEntity {
 	private Long id;
 
 	@Column(name = "company_name")
+	@NotNull(message = "Company name is mandatory")
+	@NotEmpty("message = "Company name cannot be empty")
+	@Size(min = 5, message = "Companyname should be at least 5 characters long")
 	private String companyName;
 
 	@Column(name = "email")
+	@NotNull(message = "Email is mandatory")
+	@EMail(message = "Email should be valid")
 	private String email;
 	
 	@Column(name = "strength")
+	@PositiveOrZero(message = "Strength should be a positive number or zero")
 	private Integer strength;
 	
 	@Column(name = "website_url")
