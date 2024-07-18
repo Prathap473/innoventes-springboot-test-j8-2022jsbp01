81d5f84432d20ac1064d29ecd8eb0522ebc04b4b
Prathap473 <pratapsv02@gmail.com>
2024-07-18 17:14:26 +0530

    Update Company.java

--- src/main/java/com/innoventes/test/app/entity/Company.java
+++ src/main/java/com/innoventes/test/app/entity/Company.java
@@ -30,13 +30,19 @@ public class Company extends BaseEntity {
 	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "company_seq")
 	private Long id;
 
-	@Column(name = "company_name")
+	@NotNull(message = "Company name is mandatory")
+	@NotEmpty("message = "Company name cannot be empty")
+	@Size(min = 5, message = "Companyname should be at least 5 characters long")
 	private String companyName;
 
 	@Column(name = "email")
+	@NotNull(message = "Email is mandatory")
+	@EMail(message = "Email should be valid")
 	private String email;
 	
-	@Column(name = "strength")
+	
+	@Column(name = "Strength")
+	@PositiveOrZero(message = "Strength should be a positive number or zero")
 	private Integer strength;
 	
 	@Column(name = "website_url")
